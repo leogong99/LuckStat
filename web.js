8d99197d08de620031747e15098a305a39653131
jing gong <leogong99@gmail.com>
2014-03-02 16:40:25 -0800

    Change version and project name at web.js

--- web.js
+++ web.js
@@ -2,5 +2,5 @@ var gzippo = require('gzippo');
 var express = require('express');
 var app = express(); 
 app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(gzippo.staticGzip("luckStat/dist"));
 app.listen(process.env.PORT || 5000);
